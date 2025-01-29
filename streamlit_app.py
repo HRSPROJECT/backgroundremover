5a8a96bf120c68750202c9b6b874c50eab38fd5f
HARSHEET ABHIJEET GUNDECHA <harsheetgundecha23@gmail.com>
2025-01-29 12:52:21 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from rembg import remove
+from PIL import Image
+import io
+
+st.title("Background Remover")
+
+uploaded_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+
+
+if uploaded_file is not None:
+    try:
+        input_image = Image.open(uploaded_file)
+        output_image = remove(input_image,
+                             alpha_matting=True,
+                             alpha_matting_foreground_threshold=250,
+                             alpha_matting_background_threshold=10,
+                             session=None
+                             )
+
+        # Save the image into in-memory bytes object
+        output_image_bytes = io.BytesIO()
+        output_image.save(output_image_bytes, format="PNG")
+        output_image_bytes.seek(0)
+
+
+        st.image(output_image, caption="Processed Image", use_column_width=True)
+
+        st.download_button(
+            label="Download Processed Image",
+            data=output_image_bytes,
+            file_name="output_image.png",
+            mime="image/png",
+        )
+
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
