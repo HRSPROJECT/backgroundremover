f76d664e5961cc39a84669e0f8a59bbd414e73a6
HARSHEET ABHIJEET GUNDECHA <harsheetgundecha23@gmail.com>
2025-01-29 13:31:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,15 @@ import streamlit as st
 from rembg import remove
 from PIL import Image
 import io
+import webbrowser
 
 st.title("Background Remover")
 
-uploaded_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+with st.sidebar:
+  if st.button("Explore More"):
+      webbrowser.open_new_tab("https://hrsproject.github.io/home/")
 
+uploaded_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 
 if uploaded_file is not None:
     try:
@@ -23,7 +27,6 @@ if uploaded_file is not None:
         output_image.save(output_image_bytes, format="PNG")
         output_image_bytes.seek(0)
 
-
         st.image(output_image, caption="Processed Image", use_column_width=True)
 
         st.download_button(
@@ -32,6 +35,5 @@ if uploaded_file is not None:
             file_name="output_image.png",
             mime="image/png",
         )
-
     except Exception as e:
         st.error(f"An error occurred: {e}")
